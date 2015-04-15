18eff054526668fc0ba57771611aeec64a195204
gitph101 <850867354@qq.com>
2015-04-15 10:32:50 +0800

    1.0 Establish project

---
+++ HJKnowDaily/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HJKnowDaily
+//
+//  Created by 研究院01 on 15/4/15.
+//  Copyright (c) 2015年 Loser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
